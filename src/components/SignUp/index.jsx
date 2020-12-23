88a8b883cda074f7a86417eeeaaf43567d9c47d8
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2020-12-23 09:29:23 -0500

    fixed configuration for api keys

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
