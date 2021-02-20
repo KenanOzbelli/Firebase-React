0e39a6d9f7d62616d839008982b198fabeb3f490
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2021-02-20 14:29:55 -0500

    added roles function on the singup form

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -4,6 +4,7 @@ import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
+import * as ROLES from '../../constants/roles';
 
 const SignUpPage = () => (
   <div>
@@ -17,6 +18,7 @@ const INITIAL_STATE = {
   email: '',
   passwordOne: '',
   passwordTwo: '',
+  isAdmin: false,
   error: null,
 };
 
@@ -24,7 +26,13 @@ class SignUpFormBase extends Component {
   state = { ...INITIAL_STATE };
 
   onSubmit = event => {
-    const { username, email, passwordOne } = this.state;
+    const { username, email, passwordOne, isAdmin } = this.state;
+
+    const roles = {};
+
+    if(isAdmin){
+      roles[ROLES.ADMIN] = ROLES.ADMIN;
+    }
 
     this.props.firebase
       .doCreateUserWithEmailAndPassword(email, passwordOne)
@@ -33,6 +41,7 @@ class SignUpFormBase extends Component {
         .set({
           username, 
           email,
+          roles,
         });
       })
       .then(()=> {
@@ -50,12 +59,17 @@ class SignUpFormBase extends Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  onChangeCheckbox = event => {
+    this.setState({[event.target.name] : event.target.checked});
+  };
+
   render() {
     const {
       username,
       email,
       passwordOne,
       passwordTwo,
+      isAdmin,
       error,
     } = this.state;
 
@@ -95,6 +109,15 @@ class SignUpFormBase extends Component {
           type="password"
           placeholder="Confirm Password"
         />
+        <label>
+          Admin: 
+          <input 
+            name="isAdmin"
+            type="checkbox"
+            checked={isAdmin}
+            onChange={this.onChangebox}
+          />
+        </label>
         <button disabled={isInvalid} type="submit">
           Sign Up
         </button>
