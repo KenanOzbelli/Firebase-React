93f296f0171d3f6696aed09f1fa88c669b5a2f89
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2021-05-08 13:47:08 -0400

    added new higher order component in the sessions index file

--- src/components/Session/index.jsx
+++ src/components/Session/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AuthUserContext from './context';
 import withAuthentication from './withAuthentication';
 import withAuthorization from './withAuthorization';
+import withEmailVerification from './withEmailVerification';
 
 const Session = () => (
   <div>
@@ -9,7 +10,7 @@ const Session = () => (
   </div>
 );
 
-export { AuthUserContext, withAuthentication, withAuthorization }
+export { AuthUserContext, withAuthentication, withAuthorization, withEmailVerification }
 
 export default Session;
 
