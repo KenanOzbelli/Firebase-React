ef1f6bdcc2e0549b5f2a9a4b1b2226f17ced1975
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2021-05-08 13:44:02 -0400

    conditional rendering if a user sent another verification email

--- src/components/Session/withEmailVerification.jsx
+++ src/components/Session/withEmailVerification.jsx
@@ -29,11 +29,23 @@ const withEmailVerification = Component => {
                     {authUser => 
                         needsEmailVerification(authUser)  ? (
                             <div>
+                                {this.state.isSent ? (
+                                 <p>
+                                   E-Mail confirmation sent: Check your E-Mails (Spam folder inlcuded)
+                                   for a confirmation E-Mail.
+                                   Refresh this page once you confirm your E-Mail.
+                                 </p>
+                                ):(
                                 <p>
                                     Verify your E-mail: Check your E-Mails (Might be in your spam Folder)
                                     for a confirmation E-Mail or send another confirmation E-Mail.
                                 </p>
-                                <button type="button" onClick={this.onSendEmailVerification}>
+                                )}
+
+                                 <button type="button" 
+                                    onClick={this.onSendEmailVerification}
+                                    disabled={this.state.isSent}
+                                 >
                                     Send confirmation Email
                                 </button>
                             </div>
