b542d6ea2ff0ced9999de027a2900699d077ea90
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2021-05-09 15:53:00 -0400

    added Premission for admin page so only admins can access

--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase';
+import { withAuthorization } from '../Session';
+import * as ROLES from '../../constants/roles';
 
 class Admin extends Component {
   state = {
@@ -33,6 +36,9 @@ class Admin extends Component {
     return(
       <>
         <h1>Admin</h1>
+        <p>
+          The Admin Page is accessible by every signed in admin user.
+        </p>
 
         {loading && <div> Loading... </div>}
 
@@ -59,4 +65,10 @@ class Admin extends Component {
     </ul>
   );
 
-export default withFirebase(Admin);
+const condition = authUser =>  authUser && !!authUser.roles[ROLES.ADMIN] 
+
+
+export default compose(
+  withAuthorization(condition),
+  withFirebase
+)(Admin);
